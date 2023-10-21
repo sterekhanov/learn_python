cd4b0b69a9f5c1acaa5db7cc260eef73a30f7c42
sterekhanov <58112517+sterekhanov@users.noreply.github.com>
2023-10-21 19:02:47 +0300

    lots of fun

---
+++ hello123.py
@@ -0,0 +1,14 @@
+name = 'Сергей Тереханов'
+
+first_name = 'Сергей '
+second_name = 'Тереханов'
+
+full_name = f'{first_name} {second_name}!'  
+
+print('--------------')
+print()
+print("Меня зовут ", full_name, ' ', 'Мне 44 года и я начал продолжать учить питон. Это первый файл')
+print("--------------")
+print("Длина строки", len(full_name), "символов")
+print("--------------")
+print(1/1)

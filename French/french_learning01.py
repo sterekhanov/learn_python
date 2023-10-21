b1b188055fc32aa9d268bc4e34fbcfabd919f24a
sterekhanov <58112517+sterekhanov@users.noreply.github.com>
2023-10-21 19:37:21 +0300

    Update french_learning01.py

---
+++ French/french_learning01.py
@@ -0,0 +1,9 @@
+name = 'Урок 1'
+
+first_name = 'Сергей '
+second_name = 'Тереханов'
+
+full_name = f'{first_name} {second_name}!'  
+
+lesson_01_words = ('cette', 'la salle', 'est' )
+lesson_01_phrases = ('Cette salle est très claire', 'Adèle est malade.', 'Elle a mal à la tête.', 'Claire marche', 'Elle va à la gare.', 'Elle traverse la place', 	'ette place est belle.', 'C'est Claire', 'C'est sa mère', 'Claire aime sa mère' )

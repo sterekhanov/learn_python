83bb62d1cf5755ece3307cc1136a7fbdc38a2d13
sterekhanov <58112517+sterekhanov@users.noreply.github.com>
2023-10-24 14:50:56 +0300

    another day older and deeper in debt

--- French/french_learning01.py
+++ French/french_learning01.py
@@ -3,7 +3,28 @@ second_name = 'Тереханов'
 
 full_name = f'{first_name} {second_name}!'  
 
-Words_01={1: 'cette', 2: 'la salle', 3: 'est'}
+Words_01={1: 'cette', 
+          2: 'la salle', 
+          3: 'est', 
+          4: 'tres', 
+          5: 'Clair (claire)', 
+          6: 'malade',
+          7: 'elle', 
+          8: 'la tete', 
+          9: 'elle marche', 
+          10: 'Elle traverse',
+          11: 'belle',
+          12:  'C\'est', 
+          13: 'la mere', 
+          14: 'Elle aime',
+          15: 'Marcek met',
+          16: 'la veste',
+          17: 'beige - бежевый',
+          18: 'ma', 
+          19: 'ta', 
+          20: 'la table', 
+          21: 'la chaise'
+         }
 
 Phrases_01 ={1: 'Cette salle est très claire', 
              2: 'Adèle est malade.', 
@@ -12,13 +33,12 @@ Phrases_01 ={1: 'Cette salle est très claire',
              5: 'Elle va à la gare.', 
              6: 'Elle traverse la place', 
              7: 'Cette place est belle.', 
-             8: 'C est Claire', 
-             9: 'C est sa mère', 
+             8: 'C\'est Claire', 
+             9: 'C\'est sa mère', 
              10: 'Claire aime sa mère'}
 
 
-print("Words of lesson 1: ", Words_01)
-
 print("-------------------")
-
-print("Words of lesson 1: ", Phrases_01)
+print("Words of lesson 1: ", Words_01)
+print("-------------------")
+print("Phrases of lesson 1: ", Phrases_01)

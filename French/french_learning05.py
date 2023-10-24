4e2829b891e17c93ab62e009fc7cc0611f97a6dc
sterekhanov <58112517+sterekhanov@users.noreply.github.com>
2023-10-24 17:25:19 +0300

    another day

---
+++ French/french_learning05.py
@@ -0,0 +1,44 @@
+first_name = 'Сергей '
+second_name = 'Тереханов'
+
+full_name = f'{first_name} {second_name}!'  
+
+Words_01={1: 'Elle se leve - Она встает', 
+          2: 'six - шесть', 
+          3: 'l\'heure - час', 
+          4: 'le', 
+          5: 'petit, petite - маленький', 
+          6: 'le dejeuner',
+          7: 'la petit dejeuner - завтрак', 
+          8: 'dejeuner', 
+          9: 'Je', 
+          10: 'Elle traverse',
+          11: 'belle',
+          12:  'C\'est', 
+          13: 'la mere', 
+          14: 'Elle aime',
+          15: 'Marcek met',
+          16: 'la veste',
+          17: 'beige - бежевый',
+          18: 'ma', 
+          19: 'ta', 
+          20: 'la table', 
+          21: 'la chaise'
+         }
+
+Phrases_01 ={1: 'Cette salle est très claire', 
+             2: 'Adèle est malade.', 
+             3: 'Elle a mal à la tête.', 
+             4: 'Claire marche', 
+             5: 'Elle va à la gare.', 
+             6: 'Elle traverse la place', 
+             7: 'Cette place est belle.', 
+             8: 'C\'est Claire', 
+             9: 'C\'est sa mère', 
+             10: 'Claire aime sa mère'}
+
+
+print("-------------------")
+print("Words of lesson 1: ", Words_01)
+print("-------------------")
+print("Phrases of lesson 1: ", Phrases_01)

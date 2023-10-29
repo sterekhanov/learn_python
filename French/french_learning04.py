e8b613843ff737e5c6fa87098ea79e6922e36386
sterekhanov <58112517+sterekhanov@users.noreply.github.com>
2023-10-30 00:45:55 +0300

    brand new day

---
+++ French/french_learning04.py
@@ -0,0 +1,44 @@
+first_name = 'Сергей '
+second_name = 'Тереханов'
+
+full_name = f'{first_name} {second_name}!'  
+
+Words_04={1: 'cette', 
+          2: 'la salle', 
+          3: 'est', 
+          4: 'tres', 
+          5: 'Clair (claire)', 
+          6: 'malade',
+          7: 'elle', 
+          8: 'la tete', 
+          9: 'elle marche', 
+          10: 'Elle traverse',
+          11: 'belle',
+          12:  'C\'est', 
+          13: 'la mere', 
+          14: 'Elle aime',
+          15: 'Marcek met',
+          16: 'la veste',
+          17: 'beige - бежевый',
+          18: 'ma', 
+          19: 'ta', 
+          20: 'la table', 
+          21: 'la chaise'
+         }
+
+Phrases_04 ={1: 'Elise habite Paris', 
+             2: 'Est-ce sa ville natale?', 
+             3: 'C\'est sa ville natale.', 
+             4: 'Elise quitte Paris', 
+             5: 'Elle va à Lille.', 
+             6: 'Pierre est architecte, it travaille a la mairie.', 
+             7: 'Brigitte revise les textes et la grammaire et fait des exercices', 
+             8: 'Michel lit, it lit vite. Iren dit: "Мerci, Michel"', 
+             9: '', 
+             10: ''}
+
+
+print("-------------------")
+print("Words of lesson 4: ", Words_04)
+print("-------------------")
+print("Phrases of lesson 4: ", Phrases_04)
